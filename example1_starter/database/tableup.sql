5ca7d6dd0d466c2a58a174108f29be2c328159b1
ArtCode101 <sanayut12.service@gmail.com>
2023-10-14 10:15:47 +0700

    add database

---
+++ example1_starter/database/tableup.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `account` (
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL ,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
+)
